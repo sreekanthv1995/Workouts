0e3cc4f9aa59f12ca87b174653b2b56576c70bcb
Sreekanth V <sreekanthv1995@gmail.com>
2024-08-13 23:24:39 +0530

    first commit

--- src/stack/arrayImp/MyStack.java
+++ src/stack/arrayImp/MyStack.java
@@ -1,2 +1,50 @@
-package stack.arrayImp;public class MyStack {
+package stack.arrayImp;
+
+import java.util.Arrays;
+
+public class MyStack {
+
+    int[] stack = new int[10];
+    int top;
+
+    MyStack(){
+        this.top = -1;
+    }
+
+    public void push(int data){
+        if (top == stack.length-1){
+            System.out.println("Stack is full");
+        }else {
+            stack[++top] = data;
+        }
+    }
+
+    public int pop(){
+        if (top >= 0){
+            return stack[top--];
+        }else {
+            System.out.println("Stack is Empty");
+            return -1;
+        }
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack();
+
+
+        System.out.println(Arrays.toString(myStack.stack));
+        myStack.push(40);
+        myStack.push(50);
+        myStack.push(60);
+        myStack.push(70);
+        myStack.push(80);
+        myStack.push(90);
+        myStack.push(100);
+        myStack.push(110);
+        myStack.push(120);
+        myStack.push(130);
+        System.out.println(myStack.pop());
+        System.out.println(Arrays.toString(myStack.stack));
+    }
 }
--- src/stack/linkedlistImpl/MyStack.java
+++ src/stack/linkedlistImpl/MyStack.java
@@ -1,2 +1,59 @@
-package stack.linkedlistImpl;public class MyStack {
+package stack.linkedlistImpl;
+
+public class MyStack {
+    int height;
+    Node top;
+    static class Node {
+        Node next;
+        int value;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+    public MyStack(int value){
+        top = new Node(value);
+        height = 1;
+    }
+
+    public void print(){
+        Node temp = top;
+        while (temp != null){
+            System.out.print(temp.value+ " <-");
+            temp = temp.next;
+        }
+    }
+
+    public void push(int value){
+        Node newNode = new Node(value);
+        if (top == null){
+            top = newNode;
+        }else {
+            newNode.next = top;
+            top = newNode;
+        }
+        height++;
+    }
+
+    public Node pop(){
+        if (height == 0) return null;
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+        return temp;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+        MyStack myStack = new MyStack(10);
+        myStack.push(20);
+        myStack.push(30);
+
+        myStack.print();
+        System.out.println();
+        System.out.println(myStack.pop().value);
+    }
 }
